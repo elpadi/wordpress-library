89c04e5029da9dd0d031780e5394fb6dda6face0
Carlos Padilla <padi_05@yahoo.com>
2018-12-04 17:17:28 -0500

    Editor button styling

--- src/Editor/Button.php
+++ src/Editor/Button.php
@@ -9,6 +9,7 @@ abstract class Button {
 	public function __construct() {
 		add_filter('mce_buttons', [$this, 'registerButtonFilter']);
 		add_filter('mce_external_plugins', [$this, 'registerPluginFilter']);
+		add_action('admin_enqueue_scripts', [$this, 'enqueueButtonAssets']);
 	}
 
 	public function registerButtonFilter($buttons) {
@@ -19,6 +20,9 @@ abstract class Button {
 	public function registerPluginFilter($plugin_array) {
 	   $plugin_array[$this->name] = $this->getPluginUrl();
 	   return $plugin_array;
+	}
+
+	public function enqueueButtonAssets() {
 	}
 
 	abstract protected function getPluginUrl();
