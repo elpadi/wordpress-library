b0750c9f439491efc95efda0e28146df0947ea6d
carlos (minimal) <padi_05@yahoo.com>
2019-01-22 16:51:15 -0500

    Admin theme + admin notices

---
+++ src/Admin/Notices.php
@@ -0,0 +1,26 @@
+<?php
+namespace WordpressLib\Admin;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\PhpBridgeSessionStorage;
+
+class Notices {
+
+	public function __construct() {
+		global $symfonySession;
+		$this->flash = $symfonySession->getFlashBag();
+		add_action('admin_notices', [$this, 'display']);
+	}
+
+	public function display() {
+		foreach ($this->flash->all() as $type => $msgs)
+			foreach ($msgs as $msg)
+				printf('<div class="notice notice-%s"><p>%s</p></div>', $type, $msg);
+	}
+
+	public static function wpError($wp_error) {
+		foreach ($wp_error->get_error_messages() as $msg)
+			$this->flash->add('error', $msg);
+	}
+
+}
---
+++ src/Theme/Admin.php
@@ -0,0 +1,69 @@
+<?php
+namespace WordpressLib\Theme;
+
+use WordpressLib\Posts\CustomTaxonomy;
+
+class Admin {
+
+	public function __construct($slug, $title, $pluginDir, $templateVars=[]) {
+		$this->slug = $slug;
+		$this->title = $title;
+		$this->pluginDir = $pluginDir;
+		$this->templateVars = $templateVars;
+		add_filter('admin_body_class', [$this, 'bodyClass']);
+	}
+
+	public function isAdminScreen() {
+		global $current_screen;
+		return strpos($current_screen->base, 'tome-admin') !== FALSE;
+	}
+
+	public function bodyClass($classes) {
+		if ($this->isAdminScreen()) $classes .= ' tome2-admin ';
+		return $classes;
+	}
+
+	public function addOptions($capability, $options) {
+		$this->optionsCapability = $capability;
+		add_action('admin_menu', function() use ($options, $capability) {
+			$handle = "$this->slug-settings";
+			add_menu_page("$this->title Settings", $this->title, $capability, $handle, [$this, 'optionsHTML']);
+			foreach($options as $slug => $title)
+				add_submenu_page($handle, "$this->title $title", $title, $capability, "$this->slug-$slug-settings", [$this, 'optionsHTML']);
+		});
+	}
+
+	public function optionsHTML() {
+		global $current_screen;
+		if (!current_user_can($this->optionsCapability)) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+		if (strpos($current_screen->id, "$this->slug-settings") !== FALSE) {
+			$template = 'home';
+		}
+		elseif (preg_match("/$this->slug-(.*)-settings$/", $current_screen->id, $matches)) {
+			$template = $matches[1];
+		}
+		else throw new \InvalidArgumentException("Admin screen '$current_screen->id' is invalid.");
+		$this->template($template, TRUE);
+	}
+
+	public function template($templateName, $isGlobal=FALSE) {
+		global $current_user, $post;
+		extract($this->templateVars);
+		if ($isGlobal) {
+			echo '<style>html { padding-top: 0 !important; }</style>';
+			include($this->pluginDir."/templates/global/before-content.php"); 
+		}
+		include($this->pluginDir."/templates/content/$templateName.php"); 
+		if ($isGlobal) {
+			include($this->pluginDir."/templates/global/after-content.php"); 
+		}
+	}
+
+	public function registerAssets($fn) {
+		add_action('admin_enqueue_scripts', $fn);
+		add_action('login_enqueue_scripts', $fn);
+	}
+
+}
