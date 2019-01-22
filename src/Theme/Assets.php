4b94ac2eeefefb009a9d4cfbfd8d224e67ecca4d
carlos (minimal) <padi_05@yahoo.com>
2019-01-22 16:48:06 -0500

    Whole dirs + custom paths

--- src/Theme/Assets.php
+++ src/Theme/Assets.php
@@ -8,11 +8,11 @@ class Assets {
 	protected $assetPath;
 	protected $prefix;
 
-	public function __construct($baseUri, $baseDir, $assetPath) {
+	public function __construct($prefix, $baseUri, $baseDir, $assetPath) {
+		$this->prefix = $prefix;
 		$this->baseUri = $baseUri;
 		$this->baseDir = $baseDir;
 		$this->assetPath = $assetPath;
-		$this->prefix = basename(get_stylesheet_directory());
 	}
 
 	public function asset($path, $callback, $ext='', $folder='', $deps=[], $version='') {
@@ -25,12 +25,29 @@ class Assets {
 		);
 	}
 
+	public function raw($type, $handle, $path, $deps=[], $version='') {
+		call_user_func(
+			$type == 'js' ? 'wp_enqueue_script' : 'wp_enqueue_style',
+			$handle,
+			"$this->baseUri/$this->assetPath/$path",
+			$deps,
+			empty($version) ? filemtime("$this->baseDir/$this->assetPath/$path") : $version
+		);
+	}
+
 	public function js($path, $deps=[], $version='') {
-		$this->asset($path, 'wp_enqueue_script', 'js', 'js');
+		$this->asset($path, 'wp_enqueue_script', 'js', 'js', $deps, $version);
 	}
 
 	public function css($path, $deps=[], $version='') {
-		$this->asset($path, 'wp_enqueue_style', 'css', 'css');
+		$this->asset($path, 'wp_enqueue_style', 'css', 'css', $deps, $version);
+	}
+
+	public function dir($path, $type) {
+		$base = "$this->baseDir/$this->assetPath/$type";
+		foreach (glob("$base/$path/*.$type") as $file) {
+			call_user_func([$this, $type], "$path/" . basename($file, ".$type"));
+		}
 	}
 
 }
