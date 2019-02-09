69b943981cdbe3cb0a3d1da8c7e1a21c712b7864
carlos (minimal) <padi_05@yahoo.com>
2019-02-09 13:30:55 -0500

    Moved ckEditor classes to Classic namespace

---
+++ src/Editor/Classic/Button.php
@@ -0,0 +1,30 @@
+<?php
+namespace WordpressLib\Editor\Classic;
+
+abstract class Button {
+
+	protected $name;
+	protected $icon;
+
+	public function __construct() {
+		add_filter('mce_buttons', [$this, 'registerButtonFilter']);
+		add_filter('mce_external_plugins', [$this, 'registerPluginFilter']);
+		add_action('admin_enqueue_scripts', [$this, 'enqueueButtonAssets']);
+	}
+
+	public function registerButtonFilter($buttons) {
+	   array_push($buttons, $this->name);
+	   return $buttons;
+	}
+		 
+	public function registerPluginFilter($plugin_array) {
+	   $plugin_array[$this->name] = $this->getPluginUrl();
+	   return $plugin_array;
+	}
+
+	public function enqueueButtonAssets() {
+	}
+
+	abstract protected function getPluginUrl();
+
+}
