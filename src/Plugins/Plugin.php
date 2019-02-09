2b21c26bb20bcecf59174b8a26077da9e37467a7
carlos (minimal) <padi_05@yahoo.com>
2019-02-09 13:29:50 -0500

    Plugin API for complex components

---
+++ src/Plugins/Plugin.php
@@ -0,0 +1,13 @@
+<?php
+namespace WordpressLib\Plugins;
+
+abstract class Plugin {
+
+	public function __construct() {
+		add_action('init', [$this, 'init']);
+	}
+
+	public function init() {
+	}
+
+}
---
+++ src/Plugins/Theme.php
@@ -0,0 +1,13 @@
+<?php
+namespace WordpressLib\Plugins;
+
+abstract class Theme extends Plugin {
+
+	public function __construct() {
+		parent::__construct();
+		add_filter('admin_theme_submenus', [$this, 'addSubMenus']);
+	}
+
+	abstract public function addSubMenus($submenus);
+
+}
